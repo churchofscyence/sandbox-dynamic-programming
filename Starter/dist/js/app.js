ab7755b17fc92bd427f6c5015afc09cb9cb08a96
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-12-09 22:14:31 -0500

    Install Visual Studio Code Debugger

--- Starter/dist/js/app.js
+++ Starter/dist/js/app.js
@@ -3,13 +3,16 @@ console.log('App is running!');
 $(function () {
     $('#show').on('click', function () {
         //alert("I've been clicked!");
-        const c = document.getElementById("myCanvas");
-        // @ts-ignore
-        const ctx = c.getContext("2d");
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.fillRect(20, 20, 300, 50);
-        ctx.stroke();
+        const canvas = document.getElementById("myCanvas");
+        if (typeof canvas === "object") {
+            const ctx = canvas.getContext("2d");
+            if (ctx !== null) {
+                ctx.beginPath();
+                ctx.fillStyle = "red";
+                ctx.fillRect(20, 20, 300, 50);
+                ctx.stroke();
+            }
+        }
     });
 });
 //# sourceMappingURL=app.js.map

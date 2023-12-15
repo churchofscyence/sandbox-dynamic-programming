f2aafb0cee6349df9a148ca180876499d783f33d
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-12-15 02:08:04 -0500

    init commit

--- LEETCODE/ContainerWithMostWater/dist/js/app.js
+++ LEETCODE/ContainerWithMostWater/dist/js/app.js
@@ -1,30 +1,22 @@
 "use strict";
 console.log('App is running!');
-//To install jquery locally run "npm i --save-dev @types/jquery"
 $(function () {
     $('#show').on('click', function () {
-        //alert("I've been clicked!");
         const canvas = document.getElementById("myCanvas");
         if (typeof canvas === "object") {
             const ctx = canvas.getContext("2d");
             if (ctx !== null) {
-                // Start a new Path
                 ctx.beginPath();
                 ctx.moveTo(100, 500);
                 ctx.lineTo(700, 500);
-                // Draw the Path
                 ctx.stroke();
-                // Start a new Path
                 ctx.beginPath();
                 ctx.moveTo(160, 480);
                 ctx.lineTo(160, 500);
-                // Draw the Path
                 ctx.stroke();
-                // Start a new Path
                 ctx.beginPath();
                 ctx.moveTo(220, 340);
                 ctx.lineTo(220, 500);
-                // Draw the Path
                 ctx.stroke();
             }
         }

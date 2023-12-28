fe9c249167c821bc30d3f2911be179013c0e08c6
Johnny Cowboy <johnnycowboy3033@gmail.com>
2023-12-27 19:35:28 -0500

    init commit

--- STARTER/Console/dist/solution1.js
+++ STARTER/Console/dist/solution1.js
@@ -1,11 +1,13 @@
 import { sayHello, ConsoleUtil } from "./utilities/console-util.js";
+import { MessageEnum } from "./enums/message-enum.js";
 $(document).ready(function () {
     let consoleUtil = new ConsoleUtil(3, 100);
+    let messageBlock = "#solution-1";
     for (let i = 0; i < 10; i++) {
-        $("#solution-1").before(consoleUtil.rowMessage(sayHello("Billy"), 'success-util'));
+        $(messageBlock).before(consoleUtil.rowMessage(sayHello("Jimmy"), MessageEnum.Successful));
     }
     for (let i = 0; i < 10; i++) {
-        $("#solution-1").before(consoleUtil.rowMessage(sayHello("Johnny"), 'fail-util'));
+        $(messageBlock).before(consoleUtil.rowMessage(sayHello("Johnny"), MessageEnum.Failure));
     }
 });
 //# sourceMappingURL=solution1.js.map
